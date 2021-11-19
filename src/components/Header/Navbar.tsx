1351a8c9c79169d22b6e9ea4ab0db72e044de4ac
Naresh Bhatia <naresh.archfirst@gmail.com>
2021-11-18 21:17:38 -0500

    Added feature to highlight the active link

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from './Navbar.module.css';
 
+const navItems = [
+  { title: 'Home', path: '/' },
+  { title: 'Orders', path: '/orders' },
+];
+
 export const Navbar = () => {
+  const router = useRouter();
+
   return (
     <nav className={styles.root}>
       <span className={styles.brand}>React Test Shop</span>
 
       <ul className="flex-1">
-        <li>
-          <Link href="/">
-            <a className={styles.link}>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/orders">
-            <a className={styles.link}>Orders</a>
-          </Link>
-        </li>
+        {navItems.map((item) => {
+          const { title, path } = item;
+          return (
+            <li key={title}>
+              <Link href={path}>
+                <a
+                  className={`${styles.link} ${
+                    router.pathname === path ? styles.active : ''
+                  }`}
+                >
+                  {title}
+                </a>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
